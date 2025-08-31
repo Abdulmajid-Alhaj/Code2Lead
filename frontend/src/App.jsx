4415a5e34385c76437c1b22017ae2018ad9f014b
Abdulmajid-Alhaj <abdemaged.alhaj123@gmail.com>
2025-08-31 18:18:52 +0300

    abd

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Layout>
+        <Routes>
+        </Routes>
+      </Layout>
+    </Router>
+  );
+}
+
+export default App;
